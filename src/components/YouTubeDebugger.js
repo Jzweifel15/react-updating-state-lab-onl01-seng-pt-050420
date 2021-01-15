5bb7f5b8073ae208dd8eed1927fe1a0f58be4b56
Justin R. Zweifel <jzweifel15@yahoo.com>
2021-01-15 20:28:30 +0000

    Automatically backed up by Learn

--- src/components/YouTubeDebugger.js
+++ src/components/YouTubeDebugger.js
@@ -20,7 +20,6 @@ export default class YouTubeDebugger extends Component
   handleClick = () => {
     this.setState({
       settings: {
-        ...this.state.settings,
         birate: 12,
         video: {
           resolution: "720p"
