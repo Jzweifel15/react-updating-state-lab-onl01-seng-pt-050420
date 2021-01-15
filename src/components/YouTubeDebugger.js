681b2fe5a82c6d6a74bfbb97fa73bb1a2406f631
Justin R. Zweifel <jzweifel15@yahoo.com>
2021-01-15 20:31:45 +0000

    Automatically backed up by Learn

--- src/components/YouTubeDebugger.js
+++ src/components/YouTubeDebugger.js
@@ -1,37 +1,12 @@
-import React, { Component } from "react";
-
-export default class YouTubeDebugger extends Component 
-{
-  constructor(props)
-  {
-    super(props);
-    this.state = {
-      errors: [],
-      user: null,
-      settings: {
-        bitrate: 8,
-        video: {
-          resolution: "1080p"
-        }
-      }
-    }
-  }
-
   handleClick = () => {
     this.setState({
       settings: {
-        birate: 12,
+        ...this.state.settings,
+        bitrate: 12,
         video: {
+          ...this.state.settings.video,
           resolution: "720p"
         }
       }
     });
   }
-
-  render()
-  {
-    return (
-      <button onClick={this.handleClick}></button>
-    )
-  }
-}
